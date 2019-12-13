8f99af2a026e535d4357820b7c1202693407b038
Thien Ban BUI <thienban91@hotmail.fr>
2019-12-13 12:38:02 +0100

    addUser deleteUser

--- schema/schema.js
+++ schema/schema.js
@@ -7,6 +7,8 @@ const {
     GraphQLString,
     GraphQLInt,
     GraphQLSchema,
+    GraphQLList,
+    GraphQLNonNull
 } = graphql;
 
 const CompanyType = new GraphQLObjectType({
@@ -62,6 +64,36 @@ const RootQuery = new GraphQLObjectType({
         }
     }
 });
+
+const mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        addUser: {
+            type: UserType,
+            args: {
+                firstName: { type: new GraphQLNonNull(GraphQLString) },
+                age: { type: new GraphQLNonNull(GraphQLInt) },
+                companyId: { type: GraphQLString },
+            },
+            resolve(parentValue, agrs) {
+                return axios.post('http://localhost:3000/users/', { firstName,age })
+                .then(response => response.data);
+            }
+        },
+        deleteUser: {
+            type: UserType,
+            args: {
+                id: { type: new GraphQLNonNull( GraphQLString) },
+            },
+            resolve(parentValue, { id }) {
+                return axios.delete(`http://localhost:3000/companies/${id}`)
+                .then(response => response.data);
+            }
+        },
+    }
+});
+
 module.exports = new GraphQLSchema({
-    query: RootQuery
+    query: RootQuery,
+    mutation
 });
